3a86d09ecd1e28377be9809bb3cc48692173a60e
CristianH <crishernandez08@hotmail.com>
2026-03-28 14:52:07 -0500

    UPDATE:organizando localizadores por prioridad

--- src/main/java/com/tricentis/demowebshop/UI/CarritoPageUI.java
+++ src/main/java/com/tricentis/demowebshop/UI/CarritoPageUI.java
@@ -5,39 +5,21 @@ import net.thucydides.core.annotations.findby.By;
 
 public class CarritoPageUI {
 
-    //link: Categorias menu
-    //ul[@class="list"]/li/a[contains(text(),"Computers") and @href="/computers"]
     public static final Target LNK_CATEGORIAS = Target.the("Link categorias").located(By.xpath("//ul[@class='list']/li/a[contains(text(),'Computers') and @href='/computers']"));
 
-    //link: opcion categoria equipos
-    //(//div[@class='item-box'])[1]
     public static final Target BTN_OPCION_CATEGORIA = Target.the("Opcion categoria equipos").located(By.xpath("(//div[@class='item-box'])[1]"));
 
-    //link: producto de categoria
-    //(div[@class="item-box"])[1]
     public static final Target OPCION_PRODUCTOS_CATEGORIA = Target.the("Opcion productos categoria").located(By.xpath("(//div[@class='item-box'])[1]"));//link: producto de categoria
 
-    //Button: agregar al carrito
-    //input[@type="button" and contains(@id,"add-to-cart-button")]
     public static final Target BTN_AGREGAR_CARRITO = Target.the("Boton agregar al carrito").located(By.xpath("//input[@type='button' and contains(@id,'add-to-cart-button')]"));
 
-    //Button: carrito compra
-    //a[contains(text(),"Shopping cart") and @href="/cart"]
     public static final Target BTN_CARRITO_COMPRA = Target.the("Boton carito de compra").located(By.xpath("//a[contains(text(),'Shopping cart') and @href='/cart']"));//Button: carrito compra
 
-    //Button: seleccion ciudad
-    //select[@id='CountryId']
-    public static final Target SEL_OPCION_CIUDAD= Target.the("Seleccion ciudad").located(By.xpath("//select[@id='CountryId']"));
+    public static final Target SEL_OPCION_CIUDAD= Target.the("Seleccion ciudad").located(By.id("CountryId"));
 
-    //Radio: terminos del servicio
-    //input[@id='termsofservice']
-    public static final Target RBTN_OPCION_SERVCIO_TERMINOS= Target.the("Radio button terminos y servicio").located(By.xpath("//input[@id='termsofservice']"));
+    public static final Target RBTN_OPCION_SERVCIO_TERMINOS= Target.the("Radio button terminos y servicio").located(By.id("termsofservice"));
 
-    //Botton: Chekout
-    //button[@id='checkout']
-    public static final Target BTN_CHEKOUT= Target.the("Boton Chekout").located(By.xpath(" //button[@id='checkout']"));
+    public static final Target BTN_CHEKOUT= Target.the("Boton Chekout").located(By.id("checkout"));
 
-    //Texto validacion pagina checkout
-    //h1[contains(text(),"Checkout")]
     public static final Target LBL_PAGINA_CHEKOUT = Target.the("Label confirmar pagina checkout").located(By.xpath("//h1[contains(text(),'Checkout')]"));
 }
--- src/main/java/com/tricentis/demowebshop/UI/LoginPageUI.java
+++ src/main/java/com/tricentis/demowebshop/UI/LoginPageUI.java
@@ -6,26 +6,15 @@ import net.thucydides.core.annotations.findby.By;
 public class LoginPageUI {
 
     //link: Login
-    //a[contains(text(),'Log in') or @href='/Log in']
-    public static final Target LNK_LOG_IN = Target.the("Link login").located(By.xpath("//a[contains(text(),'Log in') or @href='/Log in']"));
+    public static final Target LNK_LOG_IN = Target.the("Link login").located(By.linkText("Log in"));
 
-    //Input text: Email
-    //input[@id='Email']
-    public static final Target INPUT_TEXT_EMAIL = Target.the("Input texto email").locatedBy("//input[@id='Email']");
+    public static final Target INPUT_TEXT_EMAIL = Target.the("Input texto email").located(By.id("Email"));
 
-    //Input text: Password
-    //input[@id='Password']
-    public static final Target INPUT_TEXT_PASSWORD = Target.the("Input texto password").locatedBy("//input[@id='Password']");
+    public static final Target INPUT_TEXT_PASSWORD = Target.the("Input texto password").located(By.id("Password"));
 
-    //Boton log In:
-    //input[@type="submit" and @value="Log in"]
     public static final Target BTN_LOGIN = Target.the("Boton log in").located(By.xpath("//input[@type='submit' and @value='Log in']"));
 
-    //Texto validacion ingreso login
-    //h2[@class='topic-html-content-header']
     public static final Target LBL_LOGIN_VALIDACION = Target.the("Label confirmar login exitoso").located(By.xpath("//h2[@class='topic-html-content-header']"));
 
-    //Texto correo usuario logeado
-    //a[contains(text(),'{0}') and @href='/customer/info'] dinamico
     public static final Target LBL_EMAIL_VALIDACION = Target.the("Label confirmar correo {0} exitoso").locatedBy("//a[contains(text(),'{0}') and @href='/customer/info']");
 }
